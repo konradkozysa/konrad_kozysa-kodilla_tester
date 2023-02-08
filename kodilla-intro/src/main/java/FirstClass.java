d840a3e79f511de41ec2ed35890d6c7d7368ce46
konradkozysa <kozysa.k@gmail.com>
2023-02-08 13:48:06 +0100

    0.9 wyswietlanie a zwracanie wartosci

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,7 +2,11 @@ import javax.swing.plaf.synth.SynthTextAreaUI;
 
 public class FirstClass {
     public static void main(String[] args) {         // [2]
-        String welcome = "Hello from FirstClass!"; // [3]
+        String welcome = sayHello();
         System.out.println(welcome);
+    }
+    private static String sayHello() {
+        String text = "Hello from FirstClass!";
+        return text;
     }
 }
