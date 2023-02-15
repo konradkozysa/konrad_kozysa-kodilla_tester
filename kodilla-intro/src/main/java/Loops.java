c7c8848b50a22cf042b9f4997c2c4a4b3c4e984c
konradkozysa <kozysa.k@gmail.com>
2023-02-15 15:47:59 +0100

    0.13 Petle - while

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Loops {
 
     public static int sumNumbers(int[] numbers) {
@@ -8,9 +10,24 @@ public class Loops {
         return sum;
     }
 
+    public static int getCountOfRandomNumbers(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(10);
+            sum = sum + temp;
+            result++;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
         int result = sumNumbers(numbers);
         System.out.println(result);
+        int max = 100;
+        int count = getCountOfRandomNumbers(max);
+        System.out.println(count);
     }
 }
